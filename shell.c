fa7bff511ece8a1e6de0812ae0ff7b59b91afa90
Ciirajohn <njaujohn002@gmail.com>
2022-06-10 19:41:24 +0000

    simple shell

---
+++ shell.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* set_bit - sets the value of a bit to 1 at a given index
+* @n:  pointer to number
+* @index: index that starts from zero
+* Return: 1 if successful, -1 otherwise
+*/
+int set_bit(unsigned long int *n, unsigned int index)
+{
+
+if (index > 63)return (-1);
+*n = ((1UL << index) | *n);
+
+return (1);
+}
